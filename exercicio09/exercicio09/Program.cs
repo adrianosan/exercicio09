7c0ff0b4df28d94d5fc46075c5d8d0c1e91de8fd
adriano.santos <adriano.santos@squadra.com.br>
2019-05-29 14:30:07 -0300

    exercicio09

---
+++ exercicio09/exercicio09/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2 = 0;
+            int resultado = 0;
+            Console.Write("Digite o Primeiro Número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+            resultado = n1 + n2;
+            Console.WriteLine("A soma é {0}", resultado);
+
+
+
+        }
+
+    }
+}
